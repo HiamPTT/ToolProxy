05251919a445173d6024eb12021d9af87dbe2ef4
HiamPTT <118176243+HiamPTT@users.noreply.github.com>
2025-04-13 22:32:30 +0700

    Update shadowsocks.sh

--- shadowsocks.sh
+++ shadowsocks.sh
@@ -1,23 +1,19 @@
 #!/bin/bash
 
-# Nhận tham số từ dòng lệnh
-USER=$1
-PASS=$2
+echo -e "Start Install ShadowSocks ..."
+PASS=$1
 
 # Cài đặt Shadowsocks
-sudo apt-get update
 sudo apt-get install -y shadowsocks-libev
 
-# Tạo file cấu hình với username và password từ tham số
+# Tạo file cấu hình với password từ tham số
 cat <<EOL | sudo tee /etc/shadowsocks-libev/config.json
 {
     "server": "0.0.0.0",
-    "server_port": 1080,
-    "local_address": "127.0.0.1",
-    "local_port": 1080,
+    "server_port": 8686,
     "password": "$PASS",
     "timeout": 300,
-    "method": "aes-256-cfb"
+    "method": "chacha20-ietf-poly1305"
 }
 EOL
 
@@ -25,7 +21,4 @@ EOL
 sudo systemctl start shadowsocks-libev
 sudo systemctl enable shadowsocks-libev
 
-# Kiểm tra trạng thái
-sudo systemctl status shadowsocks-libev
-
-echo "Shadowsocks đã được cài đặt và cấu hình với user: $USER và pass: $PASS"
+echo "Shadowsocks đã được cài đặt thành công"
